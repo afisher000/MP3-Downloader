38bdacbac183efbc29ce8047046cec5a8724fd94
afisher000 <43121839+afisher000@users.noreply.github.com>
2022-07-09 10:54:27 -0700

    updates

--- itunes_api.py
+++ itunes_api.py
@@ -11,23 +11,31 @@ import re
 
 
 class API():
-    def __init__(self, search):
+    def __init__(self, search, progressbar):
         self.search_url = 'https://itunes.apple.com/search?'
         self.lookup_url = 'https://itunes.apple.com/lookup?'
 
+        self.progressbar = progressbar
+        self.progressbar.setValue(0)
+        self.progressbar.show()
         self.get_artist(search)
         self.get_albums()
         self.get_tracks()
+        self.progressbar.hide()
 
+
+        
     def get_tracks(self):
         self.tracks = {}
-        for album_id in self.albums.id:
+        for album_id in self.albums.index:
             url = f'{self.lookup_url}id={album_id}&entity=song'
             results = requests.get(url).json()['results']
             track_list = [self.clean_name(result['trackName'])
                       for result in results if result['wrapperType']=='track']
             if len(set(track_list))>4:
                 self.tracks[album_id] = track_list
+            new_progress = round(self.progressbar.value() + 80/len(self.albums))
+            self.progressbar.setValue(new_progress)
         print(f'{len(self.tracks)} important albums')
         return 
         
@@ -42,21 +50,24 @@ class API():
         
         self.artist_name = r['artistName']
         self.artist_id = r['artistId']
+        self.artist_amgid = r['amgArtistId']
+        self.progressbar.setValue(10)
         return
     
     def get_albums(self):
         url = f'{self.lookup_url}id={self.artist_id}&entity=album'
         results = requests.get(url).json()['results']
         
-        albums = pd.DataFrame(columns=['id','raw_name','name','tracks'])
+        data = {}
         for entry in results:
             if 'collectionName' in entry.keys():
-                album_id = entry['collectionId']
-                raw_name = entry['collectionName']
-                tracks = entry['trackCount']
-                name = self.clean_name(raw_name)
-                if name is not None:
-                    albums.loc[len(albums)] = [album_id, raw_name, name, tracks]
+                entry_data = {'raw_name':entry['collectionName'],
+                              'name':self.clean_name(entry['collectionName']),
+                              'tracks':entry['trackCount'],
+                              'artwork_url':entry['artworkUrl60']}
+                if entry_data['name'] is not None:
+                    data[entry['collectionId']] = entry_data      
+        albums = pd.DataFrame(data).transpose()
         print(f'{len(albums)} raw albums')
         
         # Munge duplicates
@@ -64,6 +75,7 @@ class API():
         albums.sort_values(by=['name','name_len'], ascending = True, inplace=True)
         self.albums = albums.drop_duplicates(subset=['name'])
         print(f'{len(self.albums)} clean albums')
+        self.progressbar.setValue(20)
         return 
 
     def clean_name(self, name):
@@ -74,9 +86,6 @@ class API():
         name = re.sub(' - .*','',name).strip()
         return name
         
-
-data = API('the killers')
-
 
 
 
